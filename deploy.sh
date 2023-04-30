0cebf746335178917ad464e58ec6d90a016bc0c1
harsha-syna <svattikuti@synamedia.com>
2023-04-30 13:03:07 +0530

    changed script to kill process that running on port 80

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,10 @@
 
 cd /home/ec2-user/flask-demo
 git pull origin main
-pip install -r requirements.txt
+# pip install -r requirements.txt
+# Find the PID of the process listening on port 80
+pid=$(sudo fuser 80/tcp)
+
+# Kill the process with the PID we found
+sudo kill -9 $pid
 sudo python3 app.py &
